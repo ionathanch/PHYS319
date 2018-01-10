0f20e886f61e13adac2332ceaef2e818e4699ca7
Jonathan Chan <jonathanhwchan@gmail.com>
2018-01-09 16:19:26 -0800

    Added comments for assembly

--- lab2.asm
+++ lab2.asm
@@ -8,16 +8,16 @@ START:
     mov.b   #11110111b,     &P1DIR
 
     ; set digits
-    mov.b   #01100001b,     &P1OUT
+    mov.b   #01100001b,     &P1OUT  ; xxx6
     mov.b   #01100000b,     &P1OUT
 
-    mov.b   #01000011b,     &P1OUT
+    mov.b   #01000011b,     &P1OUT  ; xx46
     mov.b   #01000010b,     &P1OUT
-ss
-    mov.b   #00010101b,     &P1OUT
+
+    mov.b   #00010101b,     &P1OUT  ; x146
     mov.b   #00010100b,     &P1OUT
 
-    mov.b   #01000111b,     &P1OUT
+    mov.b   #01000111b,     &P1OUT  ; 4146
     mov.b   #01000110b,     &P1OUT
 
     ; disable
